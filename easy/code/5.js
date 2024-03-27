baf9835800cd863224e3c4e9b0e1363917a0a944
Mr.yao <Mr.yao.email@qq.com>
2024-03-27 18:29:03 +0800

    feat: update

---
+++ easy/code/5.js
@@ -0,0 +1,21 @@
+
+
+// const str = 'A man, a plan, a canal: Panama'
+// const str = 'race a car'
+
+// 会有 超时的case  需要再研究一下
+
+function foo(str) {
+    const arr = str.toLowerCase().match(/[a-z]/g);
+    let left = 0; let right = arr.length - 1;
+    while(left !== right) {
+        if(arr[left] !== arr[right]) {
+            return 0
+        }
+        left++;
+        right--;
+    }
+    return 1
+}
+
+console.log(foo('race a car'))
